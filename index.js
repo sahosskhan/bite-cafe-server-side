099e72be8a9e852c8c98e6cfbda5dbb848661446
sahosskhan <sahosskhan359@gmail.com>
2024-04-23 06:27:04 +0600

    server base setup

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = process.env.DB_URL;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+   
+   
+   
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Bite Cafe  is running')
+})
+
+app.listen(port, () => {
+    console.log(`Bite Cafe is running on port ${port}`);
+})
